b527194111f177a2a4aa76409a1bced5ecf888ef
Luis Chang <luis.chang@outlook.com>
2021-10-25 19:16:55 -0500

    Agregar archivos de proyecto.

---
+++ S4_NETCOREMVC_JQUERY.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S4_NETCOREMVC_JQUERY.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ S4_NETCOREMVC_JQUERY.Web/Repository/CustomerRepo.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S4_NETCOREMVC_JQUERY.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S4_NETCOREMVC_JQUERY.Web.Repository
+{
+    public class CustomerRepo
+    {
+
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            using var data = new SalesDBContext();
+            return data.Customer.ToList();
+        }
+        public static IEnumerable<Customer> GetCustomersSP()
+        {
+            using var data = new SalesDBContext();
+            return data.Customer.FromSqlRaw("SP_GET_CUSTOMER");
+        }
+
+        public static async Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            using var data = new SalesDBContext();
+            return await data.Customer.ToListAsync();
+        }
+
+        public static async Task<Customer> GetCustomerById(int id)
+        {
+            using var data = new SalesDBContext();
+            return await data.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public static async Task<bool> Insert(Customer customer)
+        {
+            bool exito = true;
+            try
+            {
+                using var data = new SalesDBContext();
+                await data.Customer.AddAsync(customer);
+                await data.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+        }
+
+        public static async Task<bool> Update(Customer customer)
+        {
+            bool exito = true;
+            try
+            {
+                using var data = new SalesDBContext();
+                var customerNow = await data.Customer.Where(x => x.Id == customer.Id).FirstOrDefaultAsync();
+
+                customerNow.FirstName = customer.FirstName;
+                customerNow.LastName = customer.LastName;
+                customerNow.City = customer.City;
+                customerNow.Country = customer.Country;
+                customerNow.Phone = customer.Phone;
+
+                await data.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+            return exito;
+
+        }
+
+        public static async Task<bool> Delete(int id)
+        {
+            bool exito = true;
+
+            try
+            {
+                using var data = new SalesDBContext();
+                var customerNow = await data.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+                data.Customer.Remove(customerNow);
+                await data.SaveChangesAsync();
+
+            }
+            catch (Exception)
+            {
+                exito = false;
+            }
+
+            return exito;
+        }
+
+
+    }
+}
